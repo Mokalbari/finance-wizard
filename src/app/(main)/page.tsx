39a4694fc132696183b31af67fa08f6618b90728
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-10-11 10:53:02 +0200

    refacto grid to be in tailwind + style instead of css

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,7 +6,6 @@ import CardLayout from "./_components/card-layout"
 import TransactionSection from "./_components/transaction-section"
 import BillsSection from "./_components/bills-section"
 
-import "./styles/home-page.css"
 import {
   fetchBudgetOverview,
   fetchLatestTransactions,
@@ -33,22 +32,22 @@ export default async function Home() {
   const sections = [
     {
       key: "pots",
-      className: "cstm-grid-pots mt-8",
+      className: "row-start-1 row-end-3 col-start-1 col-end-2 mt-8",
       component: <PotsSection data={potsOverview} />,
     },
     {
       key: "transactions",
-      className: "cstm-grid-transaction mt-8",
+      className: "row-start-3 col-start-1 row-end-7 col-end-2 mt-8",
       component: <TransactionSection data={latestTransactions} />,
     },
     {
       key: "budget",
-      className: "cstm-grid-budget mt-8",
+      className: "row-start-1 col-start-2 row-end-4 col-end-3 mt-8",
       component: <BudgetSection data={budgetOverview} />,
     },
     {
       key: "bills",
-      className: "cstm-grid-bills mt-8",
+      className: "row-start-4 col-start-2 row-end-7 col-end-3 mt-8",
       component: <BillsSection data={recurringBills} />,
     },
   ]
@@ -57,7 +56,13 @@ export default async function Home() {
     <div className="mb-48 max-lg:mx-auto max-lg:max-w-[90%]">
       <PageTitle htmlTag="h1" text="Overview" className="mt-6 sm:mt-10" />
       <OverviewGrid data={userBalance} className="mt-8" />
-      <div className="cstm-grid">
+      <div
+        className="lg:grid lg:gap-6"
+        style={{
+          gridTemplateColumns: "1.1fr 0.9fr",
+          gridTemplateRows: "repeat(6, 9.375rem)",
+        }}
+      >
         {sections.map(({ key, className, component }) => (
           <CardLayout key={key} className={className}>
             {component}
