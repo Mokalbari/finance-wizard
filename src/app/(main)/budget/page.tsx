19a178cb5251a7589c72a1559ea5fcf26437a44c
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-10-14 14:17:30 +0200

    add button + add margin to page

--- src/app/(main)/budget/page.tsx
+++ src/app/(main)/budget/page.tsx
@@ -1,16 +1,18 @@
-// type Props = {}
-
 import PageTitle from "@/src/ui/shared/atoms/page-title"
 import BudgetCard from "./_components/budget-card"
 import { fetchBudgetCard } from "./actions"
 import { getUniqueID } from "@/src/lib/functions"
+import AddButton from "@/src/ui/shared/atoms/add-button"
 
 export default async function Page() {
   const budgetCategory = await fetchBudgetCard()
 
   return (
     <div>
-      <PageTitle htmlTag="h1" text="Budgets" className="" />
+      <div className="mx-4 mt-6 flex items-center justify-between">
+        <PageTitle htmlTag="h1" text="Budgets" className="" />
+        <AddButton showBefore text="Add New Budget" />
+      </div>
       {budgetCategory.map(budgetCategory => (
         <BudgetCard key={getUniqueID()} data={budgetCategory} />
       ))}
