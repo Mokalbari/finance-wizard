55d92f583e4d1a5e9bda22d7120c54375a4fbaa5
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-10-15 15:01:10 +0200

    add mobile design to pots card

--- src/app/(main)/pots/_components/pots-card.tsx
+++ src/app/(main)/pots/_components/pots-card.tsx
@@ -17,17 +17,17 @@ export default function PotsCard() {
         <Ellipsis />
       </header>
       <div className="flex items-center justify-between">
-        <div>Total Saved</div>
-        <div>$159.00</div>
+        <div className="text-sm text-grey-500">Total Saved</div>
+        <div className="text-xl font-bold">$159.00</div>
       </div>
-      <div className="h-2 w-full rounded-lg bg-beige-100">
+      <div className="mt-4 h-2 w-full rounded-lg bg-beige-100">
         <div className="h-full w-1/3 rounded-lg bg-blue" />
       </div>
-      <div className="flex items-center justify-between">
-        <div>7.95%</div>
-        <div>Target of $2,000</div>
+      <div className="mt-3 flex items-center justify-between">
+        <div className="text-xs font-bold text-grey-500">7.95%</div>
+        <div className="text-xs text-grey-500">Target of $2,000</div>
       </div>
-      <div className="flex items-center justify-evenly">
+      <div className="mt-10 flex items-center justify-evenly">
         <AddButton isBlack={false} showBefore text="Add Money" />
         <AddButton isBlack={false} showBefore={false} text="Withdraw" />
       </div>
