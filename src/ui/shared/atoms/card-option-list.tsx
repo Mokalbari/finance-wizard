6f9d5709ec4cf572135ba9ac2bd4fda92f8e19fd
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-10-16 18:56:45 +0200

    fix modal not openning properply

--- src/ui/shared/atoms/card-option-list.tsx
+++ src/ui/shared/atoms/card-option-list.tsx
@@ -4,6 +4,7 @@ import BudgetForm from "@/src/app/(main)/budget/_components/budget-form"
 import clsx from "clsx"
 import PotForm from "../modal/pot-form"
 import { useModal } from "@/src/hooks/useModal"
+import DeleteForm from "../modal/delete-form"
 
 type Props = {
   category: "Budget" | "Pots"
@@ -15,7 +16,10 @@ export default function CardOptionList({ isOpen, text, category }: Props) {
   const editModalRef = useRef<HTMLDialogElement>(null)
   const deleteModalRef = useRef<HTMLDialogElement>(null)
 
-  const { openModal, closeModal } = useModal(editModalRef)
+  const { openModal: openEditModal, closeModal: closeEditModal } =
+    useModal(editModalRef)
+  const { openModal: openDeleteModal, closeModal: closeDeleteModal } =
+    useModal(deleteModalRef)
 
   return (
     <div
@@ -25,15 +29,15 @@ export default function CardOptionList({ isOpen, text, category }: Props) {
     >
       <ul className="rounded-lg bg-white px-5 text-sm shadow-lg">
         <li className="py-3">
-          <button onClick={openModal}>Edit {text}</button>
+          <button onClick={openEditModal}>Edit {text}</button>
         </li>
         <li className="border-t border-t-grey-100 py-3 text-red">
-          Delete {text}
+          <button onClick={openDeleteModal}>Delete {text}</button>
         </li>
       </ul>
       <Modal
         ref={editModalRef}
-        close={closeModal}
+        close={closeEditModal}
         modalTitle={category === "Budget" ? "Edit Budget" : "Edit Pot"}
         modalDesc="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus  hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet."
       >
@@ -41,11 +45,11 @@ export default function CardOptionList({ isOpen, text, category }: Props) {
       </Modal>
       <Modal
         ref={deleteModalRef}
-        close={closeModal}
+        close={closeDeleteModal}
         modalTitle={category === "Budget" ? "Delete Budget" : "Delete Pot"}
-        modalDesc="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus  hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet."
+        modalDesc="Are you sure you want to delete this item? This action cannot be reversed, and all the data inside it will be removed forever"
       >
-        {category === "Budget" ? <BudgetForm /> : <PotForm />}
+        <DeleteForm />
       </Modal>
     </div>
   )
