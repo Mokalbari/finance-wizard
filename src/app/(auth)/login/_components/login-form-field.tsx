5af4473d6226939fee0c0a3114d789db59123e27
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-10-22 15:41:51 +0200

    fix css in form

--- src/app/(auth)/login/_components/login-form-field.tsx
+++ src/app/(auth)/login/_components/login-form-field.tsx
@@ -7,13 +7,13 @@ export default function LoginFormField() {
         <label htmlFor="email" className="text-xs font-bold text-grey-500">
           Email
         </label>
-        <input type="text" className="rounded-lg border border-grey-500 py-2" />
+        <input type="text" className="rounded-lg border border-grey-500 p-2" />
       </div>
       <div className="flex flex-col gap-1">
         <label htmlFor="password" className="text-xs font-bold text-grey-500">
           Password
         </label>
-        <input type="text" className="rounded-lg border border-grey-500 py-2" />
+        <input type="text" className="rounded-lg border border-grey-500 p-2" />
       </div>
     </>
   )
