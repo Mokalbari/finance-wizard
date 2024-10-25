5c7768e8344b7a25321a07167faf0ee82d78acc3
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-10-25 14:55:42 +0200

    fix fetchRecurringBills in home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -27,7 +27,7 @@ export default async function Home() {
     fetchPotsOverview(),
     fetchLatestTransactions(),
     fetchBudgetOverview(),
-    fetchRecurringBills(""),
+    fetchRecurringBills("", "Latest"),
   ])
 
   const sections = [
